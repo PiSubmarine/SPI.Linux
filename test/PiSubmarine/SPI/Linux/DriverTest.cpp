226eb600e533683dd99cb21f576dc387534dd281
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-03-25 17:44:27 +0100

    Implemented

--- test/PiSubmarine/SPI/Linux/DriverTest.cpp
+++ test/PiSubmarine/SPI/Linux/DriverTest.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <cstring>
+#include <iomanip>
 
 namespace PiSubmarine::SPI::Linux
 {
@@ -17,20 +18,18 @@ namespace PiSubmarine::SPI::Linux
     {
         Driver driver{"/dev/spidev0.0", 1000000};
 
-        for (int i = 0; i < 600; i++)
+        uint8_t reg = 7;
+        uint16_t dataOut = reg;
+        // dataOut |= (1 << 14);
+        uint16_t dataIn = 0;
+        bool ok = driver.WriteRead(reinterpret_cast<uint8_t*>(&dataOut), reinterpret_cast<uint8_t*>(&dataIn), 2);
+        if (!ok)
         {
-            uint8_t reg = 7;
-            uint16_t dataOut = reg;
-            // dataOut |= (1 << 14);
-            uint16_t dataIn = 0;
-            bool ok = driver.WriteRead(reinterpret_cast<uint8_t*>(&dataOut), reinterpret_cast<uint8_t*>(&dataIn), 2);
-            if (!ok)
-            {
-                std::string err = std::strerror(errno);
-                SCOPED_TRACE(err);
-            }
-            ASSERT_TRUE(ok);
-            sleep(1);
+            std::string err = std::strerror(errno);
+            SCOPED_TRACE(err);
         }
+        std::cout << std::hex << dataIn << std::endl;
+        ASSERT_TRUE(ok);
+        sleep(1);
     }
 }
